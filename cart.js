991838c0a645bdf142fe2fa0c7af9a9ecb2e9aa5
marcioc0sta <marciocst2@gmail.com>
2021-06-14 20:31:14 -0300

    feat: getPromotion

--- cart.js
+++ cart.js
@@ -1,8 +1,27 @@
 import data from './products.json'
+import {PROMOTION_TYPES} from "./enum";
 
 const { products } = data
 
 export const findById = id => {
   return products.find(item => item.id === id) || 'product not found'
+}
+
+export const eqCheckers = arr => {
+  return {
+    byAmount: amount => arr.filter(i => i === arr[0]).length === amount,
+    allEq: arr.every(i => i === arr[0]),
+  }
+}
+
+export const getPromotion = productsArray => {
+  const categories = productsArray.map(item => findById(item).category)
+  const check = eqCheckers(categories)
+
+  if(check.allEq) return PROMOTION_TYPES.SINGLE_LOOK
+  if(check.byAmount(2)) return PROMOTION_TYPES.DOUBLE_LOOK
+  if(check.byAmount(3)) return PROMOTION_TYPES.TRIPLE_LOOK
+
+  return PROMOTION_TYPES.FULL_LOOK
 }
 
--- cart.spec.js
+++ cart.spec.js
@@ -1,11 +1,12 @@
 import data from './products.json';
 
-import { findById } from "./cart";
+import { findById, eqCheckers, getPromotion } from "./cart";
+import {PROMOTION_TYPES} from "./enum";
 
 const { products } = data;
 
 describe('cart', () => {
-  it('should return a product based on its id', () => {
+  it('should return a product based on its id', function ()  {
     //given
     const givenId = 120
     //when
@@ -14,5 +15,37 @@ describe('cart', () => {
     //then
     expect(product).toStrictEqual(products[1])
     expect(inexistentProduct).toStrictEqual('product not found')
+  });
+  it('the equality checkers should return accordingly', function () {
+    //given
+    const array1 = [2, 2]
+    const array2 = ['a', 'a', 'b', 'c']
+    const array3 = ['a', 'b', 2]
+    //when
+    const check = eqCheckers(array1)
+    const check2 = eqCheckers((array2))
+    const check3 = eqCheckers(array3)
+    //then
+    expect(check.allEq).toBe(true)
+    expect(check2.byAmount(2)).toBe(true)
+    expect(check3.allEq).toBe(false)
+    expect(check3.byAmount(3)).toBe(false)
+  });
+  it('should get the promotions accordingly', function () {
+    //given
+    const fullLookArr = [120, 230, 310, 490]
+    const doubleLookArr = [130, 140, 230, 260]
+    const singleLookArr = [110, 120, 130, 140]
+    const trippleLookArr = [110, 130, 140, 230, 310, 330]
+    //when
+    const full = getPromotion(fullLookArr)
+    const double = getPromotion(doubleLookArr)
+    const single = getPromotion(singleLookArr)
+    const tripple = getPromotion(trippleLookArr)
+    //then
+    expect(full).toStrictEqual(PROMOTION_TYPES.FULL_LOOK)
+    expect(double).toStrictEqual(PROMOTION_TYPES.DOUBLE_LOOK)
+    expect(single).toStrictEqual(PROMOTION_TYPES.SINGLE_LOOK)
+    expect(tripple).toStrictEqual(PROMOTION_TYPES.TRIPLE_LOOK)
   });
 });
